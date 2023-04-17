9ab645efbdf705db3b341b120c476c14a1bdebb1
Dai Li <d.li@simplylive.tv>
2023-04-17 14:41:18 +0800

    fix  resotreSubmodule

--- src/commands/RestoreSubmodule.command.ts
+++ src/commands/RestoreSubmodule.command.ts
@@ -3,6 +3,9 @@
 import { ExtensionContext } from 'vscode';
 import Logger from '../UI/Logger';
 import Command from './Command';
+import { readFile } from 'fs';
+import { getWorkspacePath } from '../application/Helper';
+import { join } from 'path';
 
 /**
  * this class registers a Command to show the Output of the Logger
@@ -41,6 +44,41 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
 
 	static executeCommand(): void {
 		Logger.showOutput();
-		Command.exeCommand(RestoreSubmodule.restoreCMD);
+		let aFile=join(getWorkspacePath(), '/.gitmodules');
+		readFile(aFile, 'utf8', (err, data: string) => {
+			if (!data) {
+				return ;
+			}
+			
+			const lines = data.match(/[^\r\n]+/g) || ([] as unknown as RegExpMatchArray);
+			let foundSubmodule = false;
+			let curPath='';
+			lines.forEach((line) => {
+				/**
+[submodule "submodule/VideoFrameGPUModule"]
+	path = submodule/VideoFrameGPUModule
+url=git@bitbucket.org:id4tv/videoframegpumodule.git
+					 */				
+				if (line.includes('[submodule')) {
+					foundSubmodule = true;
+				} else if (foundSubmodule) {
+					let a = line.replace(' ', '');
+					if (a.includes("path=")) {
+						curPath = a.replace('path=', '').trim();
+					}
+					else if (a.includes('url=')) {
+						let curUrl = a.replace('path=', '').trim();
+						foundSubmodule = false;
+						curPath = join(getWorkspacePath(), curPath);
+						console.warn(curPath);
+						console.warn(curUrl);
+						let fCmd=`git submodule add ${curUrl} ${curPath}`;
+
+						Command.exeCommand(fCmd);
+					}
+				}
+			});
+
+		});
 	}
 }
